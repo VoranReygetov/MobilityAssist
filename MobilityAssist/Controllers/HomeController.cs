7297a8f07335a2e7856ece7407569af2ee61bd85
VoranReygetov <voran009@gmail.com>
2023-12-12 23:50:58 +0200

    Created connection to a database by .net Framework. Added Registration and LogIn pages.

--- MobilityAssist/Controllers/HomeController.cs
+++ MobilityAssist/Controllers/HomeController.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using MobilityAssist.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 
 namespace MobilityAssist.Controllers
 {
@@ -26,5 +28,91 @@ namespace MobilityAssist.Controllers
 
             return View();
         }
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(User objUser)
+        {
+            if (ModelState.IsValid)
+            {
+                using (MobilityAssistEntities db = new MobilityAssistEntities())
+                {
+                    var obj = db.Users.Where(a => a.email.Equals(objUser.email) && a.password.Equals(objUser.password)).FirstOrDefault();
+                    if (obj != null)
+                    {
+                        Session["UserID"] = obj.user_id.ToString();
+                        Session["FirstName"] = obj.first_name.ToString();
+                        return RedirectToAction("UserDashBoard");
+                    }
+                }
+                ViewBag.Message = "Credentials is not valid";
+            }
+            return View(objUser);
+        }
+
+        public ActionResult UserDashBoard()
+        {
+            if (Session["UserID"] != null)
+            {
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("Login");
+            }
+        }
+        public List<SelectListItem> GetRoleItems()
+        {
+            List<SelectListItem> items = new List<SelectListItem>();
+            using (MobilityAssistEntities db = new MobilityAssistEntities())
+            {
+                foreach (Role role in db.Roles)
+                {
+                    items.Add(new SelectListItem { Text = role.role_name, Value = role.role_id.ToString() });
+                }
+            }
+            return items;
+        }
+        [HttpGet]
+        public ActionResult Register()
+        {
+            User objUser = new User();
+            ViewBag.RoleType = GetRoleItems();
+            return View(objUser);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Register(User objUser, string RoleType)
+        {
+            if (ModelState.IsValid)
+            {
+                using (MobilityAssistEntities db = new MobilityAssistEntities())
+                {
+                    try
+                    {
+                        objUser.Role = db.Roles.Find(int.Parse(RoleType));
+                        db.Users.Add(objUser);
+                        db.SaveChanges();
+                        Session["UserID"] = objUser.user_id.ToString();
+                        Session["FirstName"] = objUser.first_name.ToString();
+                        return RedirectToAction("UserDashBoard");
+                    }
+                    catch (Exception)
+                    {
+                        ViewBag.Message = "Credentials is not valid";
+                    }
+                }
+                
+            }
+
+            ViewBag.RoleType = GetRoleItems();
+            return View(objUser);
+        }
+
     }
 }
--- MobilityAssist/Models/User.cs
+++ MobilityAssist/Models/User.cs
@@ -26,6 +26,7 @@ namespace MobilityAssist.Models
         public int user_role { get; set; }
         public string first_name { get; set; }
         public string last_name { get; set; }
+        public string email { get; set; }
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<Request> Requests { get; set; }
